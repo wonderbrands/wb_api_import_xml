e2b2c50cd350d054ea54479fc7152b95ab4f9596
AnibalArenas <anibal@wonderbrands.co>
2023-04-19 09:34:51 -0600

    [UPD] Modifica la conexión a MySQL y genera las consultas

--- invoice_creation.py
+++ invoice_creation.py
@@ -19,48 +19,59 @@ import base64
 import openpyxl
 import xlrd
 import pandas as pd
-import MySQLdb
+#import MySQLdb
+import mysql.connector
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-server_url  ='http://localhost:8090'
-db_name = 'yuju'
+server_url  ='https://wonderbrands-v3-7980516.dev.odoo.com'
+db_name = 'wonderbrands-v3-7980516'
 username = 'admin'
-password = 'odoo'
+password = 'admin123'
 
-print('Conectando API Odoo...........')
+print('Conectando API Odoo')
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(server_url))
 uid = common.authenticate(db_name, username, password, {})
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(server_url))
 print(common)
-#Conexion con API Google Drive
-print('Conexion a Mysql/Somos_Reyes/crawl_ml_colecta_prepare')
-print('')
+print('Conectando a Mysql - Tech')
+#DB = MySQLdb.connect('wonderbrands1.cuwd36ifbz5t.us-east-1.rds.amazonaws.com', 'will', 'RClTFPNeongrVSko', 'tech', local_infile=True)
+# Connect to MySQL database
+mydb = mysql.connector.connect(
+  host="wonderbrands1.cuwd36ifbz5t.us-east-1.rds.amazonaws.com",
+  user="anibal",
+  password="Tuy0TEZOcXAwBgtb",
+  database="tech"
+)
+mycursor = mydb.cursor()
+#mycursor.execute("SELECT folio, uuid FROM sr_uuid_walmart WHERE id IN %s", [tuple(sales_order_ids)])
+mycursor.execute("SELECT folio FROM sr_uuid_walmart")
+sales_order_records = mycursor.fetchall()
+mkp_reference = []
+for reference in sales_order_records:
+    row = reference[0]
+    mkp_reference.append(row)
 
-DB = MySQLdb.connect('wonderbrands1.cuwd36ifbz5t.us-east-1.rds.amazonaws.com', 'will', 'RClTFPNeongrVSko',
-                     'tech', local_infile=True)
-cursor = DB.cursor()
-
-DB.set_character_set('utf8mb4')
-cursor.execute('SET NAMES utf8mb4;')
-cursor.execute('SET CHARACTER SET utf8mb4;')
-cursor.execute('SET character_set_connection=utf8mb4;')
+#DB.set_character_set('utf8mb4')
+#cursor.execute('SET NAMES utf8mb4;')
+#cursor.execute('SET CHARACTER SET utf8mb4;')
+#cursor.execute('SET character_set_connection=utf8mb4;')
 
 #print('Conexion con la API de Google')
 # Define the path to the Excel file containing the orders and Read Excel file
-excel_file_path = dir_path + '/files/SO_data.xlsx'
-invoices_folder = dir_path + '/xml/'
-sale_file = pd.read_excel(excel_file_path, usecols=['id','XML name'])
-sale_id_file = sale_file['id'].tolist()
-xml_id_file = sale_file['XML name'].tolist()
-so_domain = ['id', 'in', sale_id_file]
+#excel_file_path = dir_path + '/files/SO_data.xlsx'
+#invoices_folder = dir_path + '/xml/'
+#sale_file = pd.read_excel(excel_file_path, usecols=['id','XML name'])
+#sale_id_file = sale_file['id'].tolist()
+#xml_id_file = sale_file['XML name'].tolist()
+so_domain = ['channel_order_reference', 'in', mkp_reference]
 # sale_ids = models.execute_kw(db_name, uid, password,'sale.order', 'search_read', [[so_domain]], {'fields': ['id', 'name','partner_id', 'order_line']})
 sale_ids = models.execute_kw(db_name, uid, password,'sale.order', 'search_read', [[so_domain]])
-
 if sale_ids:
     sale_id = int(sale_ids[0]['id'])
     # Create invoice for sales order
+    invoice_id = []
     for sale_order in sale_ids:
         sale_id = int(sale_order['id'])
         currency_id = int(sale_order['currency_id'][0])
@@ -123,53 +134,53 @@ if sale_ids:
             'company_id': company_id,
         }
         create_inv = models.execute_kw(db_name, uid, password, 'account.move', 'create', [invoice])
-        print('La factura de la orden: ', sale_id, 'fue creada con ID: ', create_inv)
-        move_id = create_inv
-        print('Factura creada con ID: ',move_id)
+        print('La factura de la orden: ', invoice_origin, 'fue creada con ID: ', create_inv)
+        #move_id = create_inv
+        invoice_id.append(create_inv)
 
-        dv_uuid = xml_id_file
-        for rec in os.listdir(invoices_folder):
-            xml_name = str(rec)
-            if xml_name in dv_uuid:
-                if rec.endswith('.xml'):
-                    # Read the contents of the XML file
-                    with open(os.path.join(invoices_folder, rec), 'rb') as f:
-                        xml_data = f.read()
+    dv_uuid = invoice_id
+    for rec in os.listdir(invoices_folder):
+        xml_name = str(rec)
+        if xml_name in dv_uuid:
+            if rec.endswith('.xml'):
+                # Read the contents of the XML file
+                with open(os.path.join(invoices_folder, rec), 'rb') as f:
+                    xml_data = f.read()
 
-                    xml_base64 = base64.b64encode(xml_data).decode('utf-8')
+                xml_base64 = base64.b64encode(xml_data).decode('utf-8')
 
-                    attachment_data = {
-                        'name': xml_name,
-                        'datas': xml_base64,
-                        'res_model': 'account.move',
-                        'res_id': move_id,
-                    }
+                attachment_data = {
+                    'name': xml_name,
+                    'datas': xml_base64,
+                    'res_model': 'account.move',
+                    'res_id': move_id,
+                }
 
-                    attachment_ids = models.execute_kw(db_name, uid, password, 'ir.attachment', 'create', [attachment_data])
-                    attachment_id = int(attachment_ids)
-                    #Table values edi_document
-                    #edi_format_id: 2 = CFDI 4.0
-                    values = [{
-                                'move_id': move_id,
-                                'edi_format_id': 2,
-                                'attachment_id': attachment_id,
-                                'state': 'sent',
-                                'create_uid': 1,
-                                'write_uid': 2,
-                            }]
-                    #The record in the table edi_document related to the invoice is created
-                    edi_document = models.execute_kw(db_name, uid, password, 'account.edi.document', 'create', values)
-                    print('Valores para la tabla Documentos EDI: ',values)
-                    print('Registro account.edi.document creado')
-                    #Invoice status is updated to posted
-                    time.sleep(1)
-                    upd_invoice_state = models.execute_kw(db_name, uid, password, 'account.move', 'write', [[move_id],{'state': 'posted'}])
-                    print('Se publica la factura: ', move_id)
-                    break
-                else:
-                    pass
+                attachment_ids = models.execute_kw(db_name, uid, password, 'ir.attachment', 'create', [attachment_data])
+                attachment_id = int(attachment_ids)
+                #Table values edi_document
+                #edi_format_id: 2 = CFDI 4.0
+                values = [{
+                            'move_id': move_id,
+                            'edi_format_id': 2,
+                            'attachment_id': attachment_id,
+                            'state': 'sent',
+                            'create_uid': 1,
+                            'write_uid': 2,
+                        }]
+                #The record in the table edi_document related to the invoice is created
+                edi_document = models.execute_kw(db_name, uid, password, 'account.edi.document', 'create', values)
+                print('Valores para la tabla Documentos EDI: ',values)
+                print('Registro account.edi.document creado')
+                #Invoice status is updated to posted
+                time.sleep(1)
+                upd_invoice_state = models.execute_kw(db_name, uid, password, 'account.move', 'write', [[move_id],{'state': 'posted'}])
+                print('Se publica la factura: ', move_id)
+                break
+            else:
+                pass
 else:
     print('El ID de la orden de MP: ', dv_nm_orden,'no coincide con ninguna venta en Odoo')
     pass
 
-DB.close()
+mydb.close()

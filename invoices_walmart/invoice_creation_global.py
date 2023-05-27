ede89564cf3aced77138dec9bac398a88c99db9b
AnibalArenas <anibal@wonderbrands.co>
2023-05-27 02:27:45 -0600

    [FIX] Corrige el bug de creación de facturas globales

--- invoices_walmart/invoice_creation_global.py
+++ invoices_walmart/invoice_creation_global.py
@@ -24,17 +24,20 @@ import mysql.connector
 
 #API Configuration
 dir_path = os.path.dirname(os.path.realpath(__file__))
-server_url  ='https://wonderbrands-v3-8038141.dev.odoo.com'
-db_name = 'wonderbrands-v3-8038141'
+server_url  ='https://wonderbrands-v3-8409830.dev.odoo.com'
+db_name = 'wonderbrands-v3-8409830'
 username = 'admin'
 password = 'admin123'
 
+print('-------------------------------------------------------')
 print('Conectando API Odoo')
+print('-------------------------------------------------------')
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(server_url))
 uid = common.authenticate(db_name, username, password, {})
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(server_url))
 print(common)
 print('Conectando a Mysql')
+print('-------------------------------------------------------')
 # Connect to MySQL database
 mydb = mysql.connector.connect(
   host="wonderbrands1.cuwd36ifbz5t.us-east-1.rds.amazonaws.com",
@@ -44,21 +47,34 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 #mycursor.execute("SELECT folio, uuid FROM sr_uuid_walmart WHERE id IN %s", [tuple(sales_order_ids)])
-print('Tomando el Query')
-mycursor.execute("SELECT so_name FROM sr_so_global_invoice")
+print(f"Leyendo query")
+print('-------------------------------------------------------')
+#mycursor.execute("SELECT so_name FROM sr_so_global_invoice")
+mycursor.execute("""select txn_id
+                    from bi.sr_master_orders	
+                    where out_ym = 202305	
+                        and team_name like '%walmart%'
+                        AND txn_id NOT IN (select b.order_id
+                                            from finance.sr_sat_emitidas a
+                                            left join somos_reyes.odoo_master_txns_c b
+                                                on a.folio = b.marketplace_order_id
+                                            where a.serie = 'PGA'
+                                                and date(a.fecha) BETWEEN '2023-04-01' AND '2023-05-25'
+                                            group by b.order_id)
+                    group by txn_id	
+                    order by out_timestamp_local asc
+                    limit 100""")
 sales_order_records = mycursor.fetchall()
-order_ids = []
 order_names = []
-for order in sales_order_records:
-    order_id = models.execute_kw(db_name, uid, password, 'sale.order', 'search', [[['name', '=', order]]])
-    order_ids.append(order_id[0])
-    order_names.append(order[0])
-
 try:
-    #Se crea el cuerpo de la factura con los campos necesarios
-    so_domain = ['id', 'in', order_ids]
+    for order in sales_order_records:
+        order_id = models.execute_kw(db_name, uid, password, 'sale.order', 'search_read', [[['name', '=', order]]])
+        order_names.append(order_id[0]['name'])
+
+        #Se crea el cuerpo de la factura con los campos necesarios
+    #so_domain = ['name', 'in', order_names]
     print('----------------------------------------------------------------')
-    print(f"Se creará la factura global para las siguientes SO: {order_ids}")
+    print(f"Se creará la factura global para las siguientes SO: {order_names}")
     print('----------------------------------------------------------------')
     print('Definiendo valores de la factura global')
     invoice_vals = {
@@ -69,26 +85,57 @@ try:
         'invoice_line_ids': [],
     }
     # Consultamos a sale.order para obtener los campos requeridos de cada orden de venta
-    order = models.execute_kw(db_name, uid, password, 'sale.order', 'search_read', [[so_domain]])
-    for order_line in order:
-        order_line_id = order_line['order_line']
-        for line in order_line_id:
-            sale_order_line = models.execute_kw(db_name, uid, password, 'sale.order.line', 'search_read', [[['id', '=', line]]])
-            invoice_line_vals = {
-                'display_type': sale_order_line[0]['display_type'],
-                'sequence': int(sale_order_line[0]['sequence']),
-                'name': sale_order_line[0]['name'],
-                'product_uom_id': sale_order_line[0]['product_uom'][0],
-                'product_id': sale_order_line[0]['product_id'][0],
-                'quantity': sale_order_line[0]['qty_delivered'],
-                'discount': sale_order_line[0]['discount'],
-                'price_unit': sale_order_line[0]['price_unit'],
-                'tax_ids': [(6, 0, [sale_order_line[0]['tax_id'][0]])],
-                'analytic_tag_ids': [(6, 0, sale_order_line[0]['analytic_tag_ids'])],
-                'sale_line_ids': [(4, line)],
-            }
-            invoice_vals['invoice_line_ids'].append((0, 0, invoice_line_vals))
+    for sale_order in order_names:
+        so_domain = ['name', '=', sale_order]
+        order = models.execute_kw(db_name, uid, password, 'sale.order', 'search_read', [[so_domain]])
+
+        print(f"Orden de venta encontrada")
+        order_line_id = order[0]['order_line']
+        order_name = order[0]['name']
+        order_state = order[0]['state']
+        order_inv_count = order[0]['invoice_count']
+        if order_state == 'done':
+            if order_inv_count < 1:
+                #for line in order_line_id:
+                print(f"Tomando las lineas de la orden")
+                sale_order_line = models.execute_kw(db_name, uid, password, 'sale.order.line', 'search_read', [[['id', '=', order_line_id]]])
+                for line in sale_order_line:
+                    line_id = line['id']
+                    invoice_line_vals = {
+                        'display_type': line['display_type'],
+                        'sequence': int(line['sequence']),
+                        'name': line['name'],
+                        'product_uom_id': line['product_uom'][0],
+                        'product_id': line['product_id'][0],
+                        'quantity': line['qty_delivered'],
+                        'discount': line['discount'],
+                        'price_unit': line['price_unit'],
+                        'tax_ids': [(6, 0, [line['tax_id'][0]])],
+                        'analytic_tag_ids': [(6, 0, line['analytic_tag_ids'])],
+                        'sale_line_ids': [(4, line_id)],
+                    }
+                    invoice_vals['invoice_line_ids'].append((0, 0, invoice_line_vals))
+            else:
+                print(f"La factura {order_name} ya tiene una orden creada")
+                continue
+        else:
+            print(f"Revise el estatus de la orden {order_name} está en estatus {order_state}")
+            print(f"Por lo que esta orden no puede ser facturada")
+            continue
     invoice_id = models.execute_kw(db_name, uid, password, 'account.move', 'create', [invoice_vals])
+    print(f"Agregando mensaje a la factura")
+    #Mensaje con ordenes de venta como referencia en el chatter
+    message_so = {
+        'body': order_names,
+        'message_type': 'comment',
+    }
+    write_msg_inv = models.execute_kw(db_name, uid, password, 'account.move', 'message_post', [invoice_id], message_so)
+    #Mensaje de creación por API
+    message = {
+        'body': 'Esta factura fue creada por el equipo de Tech vía API',
+        'message_type': 'comment',
+    }
+    write_msg_tech = models.execute_kw(db_name, uid, password, 'account.move', 'message_post', [invoice_id], message)
     print('----------------------------------------------------------------')
     print(f"Se creó la factura correctamente")
     print(f"El ID de la factura es el siguiente: {invoice_id}")
